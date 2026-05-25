13e3fb67658d21842f359e6e98bf638b1df410ff
KaruzO1302 <279190207+KaruzO1302@users.noreply.github.com>
2026-05-25 13:50:42 +0200

    deploy: retencja dotacje

---
+++ _next/static/chunks/app/layout-0846764730249a21.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1995:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,4400,23)),Promise.resolve().then(n.t.bind(n,2897,23))},2897:()=>{}},e=>{e.O(0,[94,400,933,146,358],()=>e(e.s=1995)),_N_E=e.O()}]);
